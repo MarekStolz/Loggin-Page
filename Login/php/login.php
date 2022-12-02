578648ce38bb52902730df4cce4786061ba29976
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2022-12-02 22:13:42 +0100

    Add files via upload

---
+++ Login/php/login.php
@@ -0,0 +1,67 @@
+<?php 
+session_start();
+
+if(isset($_POST['email']) && 
+   isset($_POST['pass'])){
+
+    include "../db_conn.php";
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $data = "email=".$email;
+    
+    if(empty($email)){
+    	$em = "Email is required";
+    	header("Location: ../login.php?error=$em&$data");
+	    exit;
+    }else if(empty($pass)){
+    	$em = "Password is required";
+    	header("Location: ../login.php?error=$em&$data");
+	    exit;
+    }else {
+
+    	$sql = "SELECT * FROM users WHERE gmail = ?";
+    	$stmt = $conn->prepare($sql);
+    	$stmt->execute([$email]);
+
+      if($stmt->rowCount() == 1){
+          $user = $stmt->fetch();
+
+          $gmail =  $user['gmail'];
+          $password =  $user['password'];
+          $fname =  $user['fname'];
+          $id =  $user['id'];
+          $type_id = $user['type_id'];
+          
+          if($gmail === $email){
+             if(password_verify($pass, $password)){
+                 $_SESSION['id'] = $id;
+                 $_SESSION['fname'] = $fname;
+
+                 header("Location: ../home.php");
+                 exit;
+             }else {
+               $em = "Incorect User name or password";
+               header("Location: ../login.php?error=$em&$data");
+               exit;
+            }
+
+          }else {
+            $em = "Incorect User name or password";
+            header("Location: ../login.php?error=$em&$data");
+            exit;
+         }
+
+      }else {
+         $em = "Incorect User name or password";
+         header("Location: ../login.php?error=$em&$data");
+         exit;
+      }
+    }
+
+
+}else {
+	header("Location: ../login.php?error=error");
+	exit;
+}
